29574c3f96df57539d7c100c486e54e7c974bfdd
bessonovaka <bessonova.en@mail.ru>
2020-05-27 15:43:45 +0300

    Adaptive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// самовызывающаяся функция для header
+// самовызывающаяся функция для header, чтобы он скользил по странице и исчезал наверху
 
 (function () {
     const header = document.querySelector('.header');
@@ -9,4 +9,18 @@
             header.classList.remove('header_active');
         }
     };
+}());
+
+// Вызов меню бургер
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    menuCloseItem = document.querySelector('.header_nav-closed');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+    });
 }());
